0309f3b26e708342251cb22abfcea2392b108470
Instructor SENA <vcortes@sena.edu.co>
2024-07-18 15:19:22 -0500

    feat(test): Add method FilterAsync() to Program - ConsoleAppTestDAL

--- Orders/ConsoleAppTestDAL/Program.cs
+++ Orders/ConsoleAppTestDAL/Program.cs
@@ -5,7 +5,9 @@ using System.Linq.Expressions;
 
 //CreateAsync().GetAwaiter().GetResult();
 //RetreiveAsync().GetAwaiter().GetResult();
-UpdateAsync().GetAwaiter().GetResult();
+//UpdateAsync().GetAwaiter().GetResult();
+FilterAsync().GetAwaiter().GetResult();
+
 
 
 Console.ReadKey();
@@ -100,4 +102,20 @@ static async Task UpdateAsync()
         }
     }
 
+}
+
+
+static async Task FilterAsync()
+{
+    using (var repository = RepositoryFactory.CreateRepository())
+    {
+        Expression<Func<Customer, bool>> criteria = c => c.Country == "USA";
+
+        var customers = await repository.FilterAsync(criteria);
+
+        foreach (var customer in customers)
+        {
+            Console.WriteLine($"Customer:  { customer.FirstName} {customer.LastName}\t from {customer.City}");
+        }
+    }
 }
